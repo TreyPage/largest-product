03a1c8e17fceb200be9f312449c1637aae84e000
Terrell Page <tpage46@gmail.com>
2019-05-29 15:38:47 -0600

    Cleaned up try/catch

--- src/edu/cnm/deepdive/Problem11.java
+++ src/edu/cnm/deepdive/Problem11.java
@@ -14,27 +14,23 @@ import java.util.List;
 public class Problem11 {
 
   private static int[][] readFile(String filename) throws IOException {
-    InputStream input = new FileInputStream(filename);
-    Reader reader = new InputStreamReader(input);
-    BufferedReader buffer = new BufferedReader(reader);
-    List<int[]> workList = new LinkedList<>();
-    String line;
-    while ((line = buffer.readLine()) != null) {
-      String[] parts = line.trim().split("\\s+");
-      int[] row = new int[parts.length];
-      for (int i = 0; i < parts.length; i++) {
-        row[i] = Integer.parseInt(parts[i]);
+    try (
+        InputStream input = new FileInputStream(filename);
+        Reader reader = new InputStreamReader(input);
+        BufferedReader buffer = new BufferedReader(reader)
+    ) {
+      List<int[]> workList = new LinkedList<>();
+      String line;
+      while ((line = buffer.readLine()) != null) {
+        String[] parts = line.trim().split("\\s+");
+        int[] row = new int[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+          row[i] = Integer.parseInt(parts[i]);
+        }
+        workList.add(row);
       }
-      workList.add(row);
+      return workList.toArray(new int[workList.size()][]);
     }
-    try {
-      buffer.close();
-      reader.close();
-      input.close();
-    } catch (IOException e) {
-      // DO NOTHING!!
-    }
-    return workList.toArray(new int[workList.size()][]);
   }
 
   public static void main(String[] args) throws IOException {
